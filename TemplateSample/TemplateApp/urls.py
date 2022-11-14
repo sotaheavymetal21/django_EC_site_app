56bf22a52ed0f42e84181f3860d0d65160e01e51
sotaheavymetal21 <sota_anegawa@pj100.biz>
2022-11-14 14:51:52 +0900

    templateを使いhtmlを表示

--- TemplateSample/TemplateApp/urls.py
+++ TemplateSample/TemplateApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "template_app"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("home", views.home, name="home"),
 ]
--- TemplateSample/TemplateApp/views.py
+++ TemplateSample/TemplateApp/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    val = "Come on!"
+    return render(request, "index.html", context={"value": val})
+
+
+def home(request):
+    my_name = "Sota Anegawa"
+    favorite_fruits = ["Apple", "Grape", "Lemon"]
+    my_info = {
+        "name": "Sota",
+        "age": 24
+    }
+    return render(request, "home.html", context={
+        "my_name": my_name,
+        "favorite_fruits": favorite_fruits,
+        "my_info": my_info
+    })
