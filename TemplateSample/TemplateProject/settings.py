b263dcc9edff6762d37c20d1a267605b7f66573e
sotaheavymetal21 <sota_anegawa@pj100.biz>
2023-01-24 12:48:19 +0900

    色々追加

--- TemplateSample/TemplateProject/settings.py
+++ TemplateSample/TemplateProject/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES = [
+    ("download", STATIC_DIR)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
